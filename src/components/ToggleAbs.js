0759e06ed94a4e2d5fa49d384962f078217a0daa
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-02-02 19:58:42 +0700

    Rework ToggleAbs.js
    Same as rework done with ToggleFinger.js

--- src/components/ToggleAbs.js
+++ src/components/ToggleAbs.js
@@ -3,60 +3,62 @@ import React, { useState } from 'react'
 function ToggleAbs({ absUpdate }) {
 
   // const key_id = ['umbi', 'llq', 'luq', 'rlq', 'ruq', 'supra', 'epi', 'all'];
+  const [absPart, setAbsPart] = useState([
+    {
+      name: 'Umbilicus',
+      id: 'umbi',
+      active: false,
+    },
+    {
+      name: 'LLQ',
+      id: 'llq',
+      active: false,
+    },
+    {
+      name: 'LUQ',
+      id: 'luq',
+      active: false,
+    },
+    {
+      name: 'RLQ',
+      id: 'rlq',
+      active: false,
+    },
+    {
+      name: 'RUQ',
+      id: 'ruq',
+      active: false,
+    },
+    {
+      name: 'Suprapubic',
+      id: 'supra',
+      active: false,
+    },
+    {
+      name: 'Epigastrium',
+      id: 'epi',
+      active: false,
+    },
+    {
+      name: 'Other',
+      id: 'all',
+      active: false,
+    },
+  ]);
 
   function handleClick(e) {
     const button_id = e.currentTarget.id;
-
-    setUmbiShown(false);
-    setLlqShown(false);
-    setLuqShown(false);
-    setRlqShown(false);
-    setRuqShown(false);
-    setSupraShown(false);
-    setEpiShown(false);
-    setAllShown(false);
-
-    switch (button_id) {
-      case 'umbi':
-        setUmbiShown(true);
-        break;
-      case 'llq':
-        setLlqShown(true);
-        break;
-      case 'luq':
-        setLuqShown(true);
-        break;
-      case 'rlq':
-        setRlqShown(true);
-        break;
-      case 'ruq':
-        setRuqShown(true);
-        break;
-      case 'supra':
-        setSupraShown(true);
-        break;
-      case 'epi':
-        setEpiShown(true);
-        break;
-      case 'all':
-        setAllShown(true);
-        break;
-      default:
-        break;
-    }
-
-    absUpdate(button_id);
-    console.log(button_id);
+    let temp = [...absPart];
+    temp.forEach(function(item) {
+      if (item.id === button_id) {
+        item.active = true;
+        absUpdate(item.name);
+      } else {
+        item.active = false;
+      }
+    })
+    setAbsPart(temp);
   }
-
-  const [umbiShown, setUmbiShown] = useState(false);
-  const [llqShown, setLlqShown] = useState(false);
-  const [luqShown, setLuqShown] = useState(false);
-  const [rlqShown, setRlqShown] = useState(false);
-  const [ruqShown, setRuqShown] = useState(false);
-  const [supraShown, setSupraShown] = useState(false);
-  const [epiShown, setEpiShown] = useState(false);
-  const [allShown, setAllShown] = useState(false);
 
   return (
     <div className="container w-3/5 m-auto mb-4 max-w-sm shadow-xl rounded-3xl">
@@ -76,7 +78,7 @@ function ToggleAbs({ absUpdate }) {
           <button id='all' className="absolute z-10 p-p3 w-w1 top-bt left-bl bg-blue-700 rounded-3xl opacity-0 hover:opacity-0" onClick={handleClick} />
         </div>
 
-        {allShown && (
+        {absPart[7].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/all-over-highlight.png')} alt='all-over' />
             <img className='absolute top-0 left-0' src={require('./img/umbilicus-highlight.png')} alt='umbilicus' />
@@ -88,43 +90,43 @@ function ToggleAbs({ absUpdate }) {
             <img className='absolute top-0 left-0' src={require('./img/epigastrium-highlight.png')} alt='epigastrium' />
           </div>
         )}
-        {umbiShown && (
+        {absPart[0].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/umbilicus-highlight.png')} alt='umbilicus' />
             <img className='absolute top-0 left-0' src={require('./img/umbilicus-active.png')} alt='umbilicus-active' />
           </div>
         )}
-        {llqShown && (
+        {absPart[1].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/llq-highlight.png')} alt='llq' />
             <img className='absolute top-0 left-0' src={require('./img/llq-active.png')} alt='llq-active' />
           </div>
         )}
-        {luqShown && (
+        {absPart[2].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/luq-highlight.png')} alt='luq' />
             <img className='absolute top-0 left-0' src={require('./img/luq-active.png')} alt='luq-active' />
           </div>
         )}
-        {rlqShown && (
+        {absPart[3].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/rlq-highlight.png')} alt='rlq' />
             <img className='absolute top-0 left-0' src={require('./img/rlq-active.png')} alt='rlq-active' />
           </div>
         )}
-        {ruqShown && (
+        {absPart[4].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/ruq-highlight.png')} alt='ruq' />
             <img className='absolute top-0 left-0' src={require('./img/ruq-active.png')} alt='ruq-active' />
           </div>
         )}
-        {supraShown && (
+        {absPart[5].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/suprapubic-highlight.png')} alt='suprapubic' />
             <img className='absolute top-0 left-0' src={require('./img/suprapubic-active.png')} alt='suprapubic-active' />
           </div>
         )}
-        {epiShown && (
+        {absPart[6].active && (
           <div>
             <img className='absolute top-0 left-0' src={require('./img/epigastrium-highlight.png')} alt='epigastrium' />
             <img className='absolute top-0 left-0' src={require('./img/epigastrium-active.png')} alt='epigastrium-active' />

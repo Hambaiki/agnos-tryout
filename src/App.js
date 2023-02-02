3517fe4389a9c9369c29cc00acf1a28c9a4702c0
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2023-02-02 21:19:13 +0700

    Minor cleanup

--- src/App.js
+++ src/App.js
@@ -56,11 +56,29 @@ function App() {
           จำเป็นต้องเลือกอย่างน้อยหนึ่งตัวเลือก
         </p>
       )}
+      {(currentStage === 1) && (
+        <ToggleAbs absUpdate={absUpdate} />
+      )}
       {(currentStage === 2) && (
         <ToggleFinger fingerUpdate={fingerUpdate} />
       )}
-      {(currentStage === 1) && (
-        <ToggleAbs absUpdate={absUpdate} />
+      {(currentStage !== 3) && (
+        <div className='container w-3/5 m-auto mt-0 max-w-sm'>
+          <button className='
+          w-full 
+          p-4 rounded-3xl 
+          bg-gray-300 
+          text-gray-500 
+          text-lg 
+          hover:bg-gradient-to-r 
+          hover:from-sky-400 
+          hover:to-blue-800
+          hover:text-white
+          transition ease-in-out duration-50'
+            onClick={progressClick}>
+            ต่อไป
+          </button>
+        </div>
       )}
       {(currentStage === 3) && (
         <div className="container w-3/5 m-auto mb-4 max-w-sm shadow-xl rounded-3xl bg-white">
@@ -84,24 +102,6 @@ function App() {
           transition ease-in-out duration-50'
             onClick={progressClick}>
             ทำใหม่อีกครั้ง
-          </button>
-        </div>
-      )}
-      {(currentStage !== 3) && (
-        <div className='container w-3/5 m-auto mt-0 max-w-sm'>
-          <button className='
-          w-full 
-          p-4 rounded-3xl 
-          bg-gray-300 
-          text-gray-500 
-          text-lg 
-          hover:bg-gradient-to-r 
-          hover:from-sky-400 
-          hover:to-blue-800
-          hover:text-white
-          transition ease-in-out duration-50'
-            onClick={progressClick}>
-            ต่อไป
           </button>
         </div>
       )}
